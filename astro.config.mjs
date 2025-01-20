cab3854ea979cb1ba3758d62c59af8d43cac8621
BOJO <asdfgh20220610@163.com>
2025-01-20 19:33:55 +0800

    补充readme.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  integrations: [],
   site: 'https://asdfgh20220610.github.io',
   experimental: {
     assets: false
